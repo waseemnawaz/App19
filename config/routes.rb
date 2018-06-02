d89a3f4b30437498aa138db2e1faa53151609ed6
waseemnawaz <waseemhijodenawaz@yahoo.es>
2018-06-02 19:20:12 +0200

    policy banner

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
 
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
-  root 'simple_pages#index'
+  #root 'simple_pages#index'
 
-  #root 'simple_pages#landing_page'
+  root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
